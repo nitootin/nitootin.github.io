084d6485ada8a20fe73efbe71b7201208a8a4199
nitootin <phillip.profissional@gmail.com>
2025-08-04 20:49:43 -0300

    h3 com texto legal

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>My fisrt page in github pages - phillip medeiros</h2>
+      <h3>testo legal</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
